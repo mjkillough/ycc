40224582a011042c172cad5dbdcad34da841cd1d
Michael Killough <michaeljkillough@gmail.com>
2020-05-15 16:51:03 +0100

    gen: push everything into state.

--- src/gen.c
+++ src/gen.c
@@ -9,98 +9,100 @@
 #include "ident.h"
 #include "map.h"
 
-typedef struct {
+struct state {
+    FILE *f;
     struct map *env;
     size_t stack_idx;
     uint64_t label_idx;
-} state_t;
+};
 
-static size_t var_idx(state_t *state, const struct ident *ident) {
-    return (size_t)map_get(state->env, ident);
+static size_t
+var_idx(struct state *s, const struct ident *ident) {
+    return (size_t)map_get(s->env, ident);
 }
 
-static bool gen_expr(FILE *f, state_t *state, ast_expr_t *expr) {
+static bool gen_expr(struct state *s, ast_expr_t *expr) {
     switch (expr->discrim) {
     case Ast_Expr_Constant:
-        fprintf(f, "mov $%s, %%rax\n", expr->str);
+        fprintf(s->f, "mov $%s, %%rax\n", expr->str);
         break;
     case Ast_Expr_Var:
-        fprintf(f, "mov -%zu(%%rbp), %%rax\n", var_idx(state, expr->ident));
+        fprintf(s->f, "mov -%zu(%%rbp), %%rax\n", var_idx(s, expr->ident));
         break;
     case Ast_Expr_BinOp:
-        gen_expr(f, state, expr->rhs);
-        fprintf(f, "pushq %%rax\n");
-        gen_expr(f, state, expr->lhs);
-        fprintf(f, "popq %%rcx\n");
+        gen_expr(s, expr->rhs);
+        fprintf(s->f, "pushq %%rax\n");
+        gen_expr(s, expr->lhs);
+        fprintf(s->f, "popq %%rcx\n");
 
         switch (expr->binop) {
         case Ast_BinOp_Addition:
-            fprintf(f, "add %%rcx, %%rax\n");
+            fprintf(s->f, "add %%rcx, %%rax\n");
             break;
         case Ast_BinOp_Subtraction:
-            fprintf(f, "sub %%rcx, %%rax\n");
+            fprintf(s->f, "sub %%rcx, %%rax\n");
             break;
         case Ast_BinOp_Multiplication:
-            fprintf(f, "imul %%rcx, %%rax\n");
+            fprintf(s->f, "imul %%rcx, %%rax\n");
             break;
         case Ast_BinOp_Division:
-            fprintf(f, "mov $0, %%rdx\n");
-            fprintf(f, "idiv %%rcx\n");
+            fprintf(s->f, "mov $0, %%rdx\n");
+            fprintf(s->f, "idiv %%rcx\n");
             break;
         case Ast_BinOp_Equal:
-            fprintf(f, "cmp %%rcx, %%rax\n");
-            fprintf(f, "mov $0, %%rax\n");
-            fprintf(f, "sete %%al\n");
+            fprintf(s->f, "cmp %%rcx, %%rax\n");
+            fprintf(s->f, "mov $0, %%rax\n");
+            fprintf(s->f, "sete %%al\n");
             break;
         case Ast_BinOp_NotEqual:
-            fprintf(f, "cmp %%rcx, %%rax\n");
-            fprintf(f, "mov $0, %%rax\n");
-            fprintf(f, "setne %%al\n");
+            fprintf(s->f, "cmp %%rcx, %%rax\n");
+            fprintf(s->f, "mov $0, %%rax\n");
+            fprintf(s->f, "setne %%al\n");
             break;
         case Ast_BinOp_LessThan:
-            fprintf(f, "cmp %%rcx, %%rax\n");
-            fprintf(f, "mov $0, %%rax\n");
-            fprintf(f, "setl %%al\n");
+            fprintf(s->f, "cmp %%rcx, %%rax\n");
+            fprintf(s->f, "mov $0, %%rax\n");
+            fprintf(s->f, "setl %%al\n");
             break;
         case Ast_BinOp_LessThanEqual:
-            fprintf(f, "cmp %%rcx, %%rax\n");
-            fprintf(f, "mov $0, %%rax\n");
-            fprintf(f, "setle %%al\n");
+            fprintf(s->f, "cmp %%rcx, %%rax\n");
+            fprintf(s->f, "mov $0, %%rax\n");
+            fprintf(s->f, "setle %%al\n");
             break;
         case Ast_BinOp_GreaterThan:
-            fprintf(f, "cmp %%rcx, %%rax\n");
-            fprintf(f, "mov $0, %%rax\n");
-            fprintf(f, "setg %%al\n");
+            fprintf(s->f, "cmp %%rcx, %%rax\n");
+            fprintf(s->f, "mov $0, %%rax\n");
+            fprintf(s->f, "setg %%al\n");
             break;
         case Ast_BinOp_GreaterThanEqual:
-            fprintf(f, "cmp %%rcx, %%rax\n");
-            fprintf(f, "mov $0, %%rax\n");
-            fprintf(f, "setge %%al\n");
+            fprintf(s->f, "cmp %%rcx, %%rax\n");
+            fprintf(s->f, "mov $0, %%rax\n");
+            fprintf(s->f, "setge %%al\n");
             break;
         }
         break;
     case Ast_Expr_UnOp:
         switch (expr->unop) {
         case Ast_UnOp_Negation:
-            gen_expr(f, state, expr->lhs);
-            fprintf(f, "neg %%rax\n");
+            gen_expr(s, expr->lhs);
+            fprintf(s->f, "neg %%rax\n");
             break;
         case Ast_UnOp_AddressOf:
             if (expr->lhs->discrim != Ast_Expr_Var) {
                 printf("error: can only take address of variables\n");
                 exit(-1);
             }
-            fprintf(f, "mov %%rbp, %%rax\n");
-            fprintf(f, "sub $%zu, %%rax\n", var_idx(state, expr->lhs->ident));
+            fprintf(s->f, "mov %%rbp, %%rax\n");
+            fprintf(s->f, "sub $%zu, %%rax\n", var_idx(s, expr->lhs->ident));
             break;
         case Ast_UnOp_Deref:
             if (expr->lhs->discrim != Ast_Expr_Var) {
                 printf("error: can only take address of variables\n");
                 exit(-1);
             }
-            fprintf(f, "mov -%zu(%%rbp), %%rax\n",
-                    var_idx(state, expr->lhs->ident));
-            fprintf(f, "mov (%%rax), %%rax\n");
+            fprintf(s->f, "mov -%zu(%%rbp), %%rax\n",
+                    var_idx(s, expr->lhs->ident));
+            fprintf(s->f, "mov (%%rax), %%rax\n");
             break;
         }
         break;
@@ -110,41 +112,41 @@ static bool gen_expr(FILE *f, state_t *state, ast_expr_t *expr) {
             exit(-1);
         }
 
-        gen_expr(f, state, expr->rhs);
+        gen_expr(s, expr->rhs);
 
         switch (expr->assignop) {
         case Ast_AssignOp_Assign:
-            fprintf(f, "mov %%rax, ");
+            fprintf(s->f, "mov %%rax, ");
             break;
         case Ast_AssignOp_Addition:
-            fprintf(f, "add %%rax, ");
+            fprintf(s->f, "add %%rax, ");
             break;
         case Ast_AssignOp_Subtraction:
-            fprintf(f, "sub %%rax, ");
+            fprintf(s->f, "sub %%rax, ");
             break;
         case Ast_AssignOp_Multiplication:
-            fprintf(f, "mov %%rax, %%rcx\n");
-            fprintf(f, "mov -%zu(%%rbp), %%rax\n",
-                    var_idx(state, expr->lhs->ident));
-            fprintf(f, "imul %%rcx, %%rax\n");
-            fprintf(f, "mov %%rax, ");
+            fprintf(s->f, "mov %%rax, %%rcx\n");
+            fprintf(s->f, "mov -%zu(%%rbp), %%rax\n",
+                    var_idx(s, expr->lhs->ident));
+            fprintf(s->f, "imul %%rcx, %%rax\n");
+            fprintf(s->f, "mov %%rax, ");
             break;
         case Ast_AssignOp_Division:
-            fprintf(f, "mov %%rax, %%rcx\n");
-            fprintf(f, "mov $0, %%rdx\n");
-            fprintf(f, "mov -%zu(%%rbp), %%rax\n",
-                    var_idx(state, expr->lhs->ident));
-            fprintf(f, "idiv %%rcx\n");
-            fprintf(f, "mov %%rax, ");
+            fprintf(s->f, "mov %%rax, %%rcx\n");
+            fprintf(s->f, "mov $0, %%rdx\n");
+            fprintf(s->f, "mov -%zu(%%rbp), %%rax\n",
+                    var_idx(s, expr->lhs->ident));
+            fprintf(s->f, "idiv %%rcx\n");
+            fprintf(s->f, "mov %%rax, ");
             break;
         }
 
-        fprintf(f, "-%zu(%%rbp)\n", var_idx(state, expr->lhs->ident));
+        fprintf(s->f, "-%zu(%%rbp)\n", var_idx(s, expr->lhs->ident));
     }
     return true;
 }
 
-static bool gen_block(FILE *f, state_t *state, ast_block_t *block);
+static bool gen_block(struct state *s, ast_block_t *block);
 
 static struct ident *_declarator_ident(struct ast_declarator *declarator) {
     switch (declarator->kind) {
@@ -153,69 +155,68 @@ static struct ident *_declarator_ident(struct ast_declarator *declarator) {
     }
 }
 
-static bool gen_statement(FILE *f, state_t *state, ast_statement_t *stmt) {
+static bool gen_statement(struct state *s, ast_statement_t *stmt) {
     switch (stmt->kind) {
     case Ast_Statement_Return:
-        gen_expr(f, state, stmt->expr);
-        fprintf(f, "mov %%rbp, %%rsp\n");
-        fprintf(f, "popq %%rbp\n");
-        fprintf(f, "ret\n");
+        gen_expr(s, stmt->expr);
+        fprintf(s->f, "mov %%rbp, %%rsp\n");
+        fprintf(s->f, "popq %%rbp\n");
+        fprintf(s->f, "ret\n");
         break;
     case Ast_Statement_If:
-        gen_expr(f, state, stmt->expr);
-        fprintf(f, "cmp $0, %%rax\n");
-        size_t end_label = state->label_idx++;
+        gen_expr(s, stmt->expr);
+        fprintf(s->f, "cmp $0, %%rax\n");
+        size_t end_label = s->label_idx++;
         if (stmt->arm2 != NULL) {
-            size_t else_label = state->label_idx++;
-            fprintf(f, "je if_%zu\n", else_label);
-            gen_statement(f, state, stmt->arm1);
-            fprintf(f, "je if_%zu\n", end_label);
-            fprintf(f, "if_%zu:\n", else_label);
-            gen_statement(f, state, stmt->arm2);
+            size_t else_label = s->label_idx++;
+            fprintf(s->f, "je if_%zu\n", else_label);
+            gen_statement(s, stmt->arm1);
+            fprintf(s->f, "je if_%zu\n", end_label);
+            fprintf(s->f, "if_%zu:\n", else_label);
+            gen_statement(s, stmt->arm2);
         } else {
-            fprintf(f, "je if_%zu\n", end_label);
-            gen_statement(f, state, stmt->arm1);
+            fprintf(s->f, "je if_%zu\n", end_label);
+            gen_statement(s, stmt->arm1);
         }
-        fprintf(f, "if_%zu:\n", end_label);
+        fprintf(s->f, "if_%zu:\n", end_label);
         break;
     case Ast_Statement_Block:
-        gen_block(f, state, stmt->block);
+        gen_block(s, stmt->block);
         break;
     case Ast_Statement_Expr:
-        gen_expr(f, state, stmt->expr);
+        gen_expr(s, stmt->expr);
         break;
     }
 
-    fprintf(f, "\n");
+    fprintf(s->f, "\n");
 
     return true;
 }
 
-static bool gen_declaration(FILE *f, state_t *state,
-                            struct ast_declaration *decl) {
+static bool gen_declaration(struct state *s, struct ast_declaration *decl) {
     for (size_t i = 0; i < decl->ndeclarators; i++) {
         if (decl->exprs[i] != NULL) {
-            gen_expr(f, state, decl->exprs[i]);
+            gen_expr(s, decl->exprs[i]);
         }
         // TODO: We can avoid the push and just decrement the stack pointer if
         // we don't have an expr.
-        fprintf(f, "pushq %%rax\n");
-        map_insert(state->env, _declarator_ident(&decl->declarators[i]),
-                   (void *)state->stack_idx);
-        state->stack_idx += 8;
+        fprintf(s->f, "pushq %%rax\n");
+        map_insert(s->env, _declarator_ident(&decl->declarators[i]),
+                   (void *)s->stack_idx);
+        s->stack_idx += 8;
     }
 
     return true;
 }
 
-static bool gen_block(FILE *f, state_t *state, ast_block_t *block) {
+static bool gen_block(struct state *s, ast_block_t *block) {
     for (size_t i = 0; i < block->nitems; i++) {
         switch (block->items[i].kind) {
         case Ast_BlockItem_Statement:
-            gen_statement(f, state, &block->items[i].stmt);
+            gen_statement(s, &block->items[i].stmt);
             break;
         case Ast_BlockItem_Declaration:
-            gen_declaration(f, state, &block->items[i].decl);
+            gen_declaration(s, &block->items[i].decl);
             break;
         }
     }
@@ -224,15 +225,16 @@ static bool gen_block(FILE *f, state_t *state, ast_block_t *block) {
 }
 
 static bool gen_function(FILE *f, ast_function_t *func) {
-    state_t state = {
+    struct state s = {
+        .f = f,
         .env = map_new(map_key_pointer),
         .stack_idx = 8,
     };
-    fprintf(f, " .globl %s\n", ident_to_str(func->ident));
-    fprintf(f, "%s:\n", ident_to_str(func->ident));
-    fprintf(f, "pushq %%rbp\n");
-    fprintf(f, "mov %%rsp, %%rbp\n");
-    return gen_block(f, &state, &func->block);
+    fprintf(s.f, " .globl %s\n", ident_to_str(func->ident));
+    fprintf(s.f, "%s:\n", ident_to_str(func->ident));
+    fprintf(s.f, "pushq %%rbp\n");
+    fprintf(s.f, "mov %%rsp, %%rbp\n");
+    return gen_block(&s, &func->block);
 }
 
 static bool gen_function_iter(void *context, const void *key, void *value) {
